897926fb6bd10a0c689270390ad7290bb7311d09
GeroA6 <aguerogeronimo954@gmail.com>
2026-06-08 12:08:35 -0300

    Creación de Enums (EstadoPago y TipoPago)

---
+++ src/main/java/com/gym/manager/model/EstadoPago.java
@@ -0,0 +1,10 @@
+package com.gym.manager.model;
+
+/**
+ * Representa la situación actual de un registro de pago en el sistema.
+ */
+public enum EstadoPago {
+    PAGADO,
+    PENDIENTE,
+    VENCIDO
+}
