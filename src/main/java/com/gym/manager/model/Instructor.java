323cc318598ada46f98f6068e4013321c0ee1c09
JennDev <ajenniferlucia@gmail.com>
2026-06-09 23:45:02 -0300

    refactor: actualizacion en Instructor para heredar Persona y mejorara POO

--- src/main/java/com/gym/manager/model/Instructor.java
+++ src/main/java/com/gym/manager/model/Instructor.java
@@ -4,32 +4,18 @@ package com.gym.manager.model;
  * Representa a un instructor dentro del sistema del gimnasio.
  * Contiene la información personal y laboral del empleado.
  */
-public class Instructor {
-    private int id;
-    private String nombre;
-    private String apellido;
+public class Instructor extends Persona {
     private String especialidad;
     private double sueldo;
 
-    public Instructor(int id, String nombre, String apellido, String especialidad, double sueldo) {
-        this.id = id;
-        this.nombre = nombre;
-        this.apellido = apellido;
+     public Instructor(int id, String nombre, String apellido, String dni, String email, String telefono, String especialidad, double sueldo) {
+        // Llama al constructor de la clase abstracta Persona de Maia
+        super(id, nombre, apellido, dni, email, telefono);
         this.especialidad = especialidad;
         this.sueldo = sueldo;
     }
-
     // Getters y Setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
-
-    public String getApellido() { return apellido; }
-    public void setApellido(String apellido) { this.apellido = apellido; }
-
-    public String getEspecialidad() { return especialidad; }
+     public String getEspecialidad() { return especialidad; }
     public void setEspecialidad(String especialidad) { this.especialidad = especialidad; }
     
     public double getSueldo() { return sueldo; }
